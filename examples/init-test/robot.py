a733ebd194777d84af8eda582e915a7fe3aeaf32
picode98 <saaman1377@gmail.com>
2020-03-09 01:43:00 -0400

    Fix module naming; cleanup example

--- examples/init-test/robot.py
+++ examples/init-test/robot.py
@@ -1,11 +1,10 @@
-import wpilib
-from adis16448._adis16448 import ADIS16448_IMU
-from os import system
+from wpilib import run, IterativeRobot, SPI
+from adis16448 import ADIS16448_IMU
 
-class MyRobot(wpilib.IterativeRobot):
+class MyRobot(IterativeRobot):
     def robotInit(self):
         print('Starting gyro init...')
-        self.myGyro = ADIS16448_IMU()
+        self.myGyro = ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, SPI.Port.kMXP, 4)
         print('Gyro init completed.')
 
     def teleopInit(self):
@@ -17,5 +16,4 @@ class MyRobot(wpilib.IterativeRobot):
         pass
 
 if __name__ == "__main__":
-    system("pause")
-    wpilib.run(MyRobot)
+    run(MyRobot)
